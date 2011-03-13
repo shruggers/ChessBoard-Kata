18edd54d4a8b55389f0f7e3df1ebe518ab5534e7
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-03-13 12:02:25 +0000

    Set up Cucumber

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source "http://rubygems.org"
+
+gem "bundler"
+
+group :development do
+  gem "autotest-standalone"
+end
+
+group :darwin do
+  gem "autotest-fsevent"
+end
+
+group :linux do
+  gem "autotest-inotify"
+end
+
+group :test do
+  gem "cucumber"
+  gem "rspec"
+end
---
+++ features/step_definitions/chess_steps.rb
@@ -0,0 +1,72 @@
+Given %r/^I have a White Pawn at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^I have a Black Knight at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+When %r/^I move the Pawn to ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^I should be warned of an illegal move message$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When %r/^I move the Knight to ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^I should be shown "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the valid moves are$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the game has just started$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the Pawn is on ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the Knight is at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^Pawn should be at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the game has not just started$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given %r/^the valid moves are ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+When %r/^the Pawn moves to ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^the Knight should be taken$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^the Pawn should be at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^the Knight should be at ([A-Z][0-9])$/ do |square|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^the Pawn should be taken$/ do
+  pending # express the regexp above with the code you wish you had
+end
