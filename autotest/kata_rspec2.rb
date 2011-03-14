63bbe6d694c8c9b95c58e737ecbe3e03969a4ecc
Jon Rowe <hello@jonrowe.co.uk>
2011-03-14 19:07:14 +0000

    setup

--- autotest/kata_rspec2.rb
+++ autotest/kata_rspec2.rb
@@ -4,7 +4,7 @@ class Autotest::KataRspec2 < Autotest::Rspec2
   def setup_rspec_project_mappings
     add_exception %r%^\./(?:doc|log|public|tmp|\.git|\.hg|\.svn|_darcs|framework|gems|\.DS_Store|autotest|bin|.*\.sqlite3|.*\.thor)%
     %w[.svn .hg .git _darcs .autotest].each { |exception| add_exception(exception) }
-    
+
     clear_mappings
 
     # Just the defaults from RSpec for now
